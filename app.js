dcc0058368b1d01b2a0b38ce5e2834cc82e474fb
Omar <588824omar@gmail.com>
2022-09-08 19:32:17 -0400

    Completed portfolio section

--- app.js
+++ app.js
@@ -35,6 +35,13 @@ function PageTransition() {
 
     })
 
+    //theme switch
+    const themeBtn = document.querySelector('.theme-btn');
+    themeBtn.addEventListener('click', () => {
+        let element = document.body;
+        element.classList.toggle('light-mode')
+    })
+
 }
 
 PageTransition();
